92b6666d26a6e6d1bb4e0507bf2911ec963292b8
Unknown <thehofch@gmail.com>
2018-05-04 09:41:05 +0300

    bootstrap + slim

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,7 +1,7 @@
 class CoverUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -29,16 +29,18 @@ class CoverUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
+  process resize_to_fill: [500, 500]
+
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [100, 100]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
