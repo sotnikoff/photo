c0ef9e332e30c6c355db4c056beafcb5317d16ae
Unknown <thehofch@gmail.com>
2018-05-06 00:34:35 +0300

    basic add

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :mentors
-  root 'pages#index'
-  devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  devise_for :mentors
+  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
 
+  root 'pages#index'
+  get '/my', to: 'pages#my'
   resources :course_categories, only: %i(index show)
+  scope 'my' do
+    resources :user_tasks, only: %i(show)
+  end
 end
