58906cb156ff1c7495957070ea2858a9f50ba8be
Unknown <thehofch@gmail.com>
2018-05-08 08:47:31 +0300

    controller specs fix

--- spec/controllers/course_categories_controller_spec.rb
+++ spec/controllers/course_categories_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CourseCategoriesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'without course category' do
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  context 'with course category' do
+    before(:each) do
+      @course_category = create :course_category
+    end
+
+    describe 'GET #show' do
+      it 'returns http success' do
+        get :show, params: { id: @course_category.id }
+        expect(response).to be_successful
+      end
+    end
+  end
 end
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CoursesController, type: :controller do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'without course' do
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to be_successful
+      end
+    end
+  end
+
+  context 'with course' do
+    before(:each) do
+      @course = create :course
+    end
+
+    describe 'GET #show' do
+      it 'returns http success' do
+        get :show, params: { id: @course.id }
+        expect(response).to be_successful
+      end
+    end
+  end
 end
